8c3361c2296582e4efe4d17bcbbd647125f75c77
GitHub Action <action@github.com>
2026-02-12 13:42:55 +0000

    build: update bundled dist files

---
+++ dist/sdk/liquid-extensions-YDIIH33Q.mjs
@@ -0,0 +1,25 @@
+import {
+  ReadFileTag,
+  configureLiquidWithExtensions,
+  createExtendedLiquid,
+  init_liquid_extensions,
+  sanitizeLabel,
+  sanitizeLabelList,
+  withPermissionsContext
+} from "./chunk-XJQKTK6V.mjs";
+import "./chunk-25IC7KXZ.mjs";
+import "./chunk-VF6XIUE4.mjs";
+import "./chunk-VPC3QSPW.mjs";
+import "./chunk-SZXICFQ3.mjs";
+import "./chunk-UCMJJ3IM.mjs";
+import "./chunk-J7LXIPZS.mjs";
+init_liquid_extensions();
+export {
+  ReadFileTag,
+  configureLiquidWithExtensions,
+  createExtendedLiquid,
+  sanitizeLabel,
+  sanitizeLabelList,
+  withPermissionsContext
+};
+//# sourceMappingURL=liquid-extensions-YDIIH33Q.mjs.map
--- dist/sdk/sdk.mjs
+++ dist/sdk/sdk.mjs
@@ -1,7 +1,7 @@
 import {
   StateMachineExecutionEngine,
   init_state_machine_execution_engine
-} from "./chunk-D2KB4MC2.mjs";
+} from "./chunk-T67JI6QF.mjs";
 import "./chunk-LG4AUKHB.mjs";
 import "./chunk-KFKHU6CM.mjs";
 import "./chunk-B7BVQM5K.mjs";
@@ -13,11 +13,11 @@ import {
   init_config
 } from "./chunk-UCNT3PDT.mjs";
 import "./chunk-NCWIZVOT.mjs";
-import "./chunk-UCHQUCML.mjs";
+import "./chunk-5BSFUV6L.mjs";
 import "./chunk-HOKQOO3G.mjs";
 import "./chunk-YJRBN3XS.mjs";
 import "./chunk-JL7JXCET.mjs";
-import "./chunk-5RGNEFYL.mjs";
+import "./chunk-XJQKTK6V.mjs";
 import "./chunk-25IC7KXZ.mjs";
 import "./chunk-VF6XIUE4.mjs";
 import "./chunk-VPC3QSPW.mjs";
