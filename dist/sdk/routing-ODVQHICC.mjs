b06d244c0580c243801a29bbeae739a8198bea09
GitHub Action <action@github.com>
2026-03-04 05:56:53 +0000

    build: update bundled dist files

---
+++ dist/sdk/routing-ODVQHICC.mjs
@@ -0,0 +1,25 @@
+import {
+  checkLoopBudget,
+  evaluateGoto,
+  evaluateTransitions,
+  handleRouting,
+  init_routing
+} from "./chunk-CZOKADR7.mjs";
+import "./chunk-LH3PEGKM.mjs";
+import "./chunk-NE3OOOB2.mjs";
+import "./chunk-JL7JXCET.mjs";
+import "./chunk-ZUEQNCKB.mjs";
+import "./chunk-25IC7KXZ.mjs";
+import "./chunk-LW3INISN.mjs";
+import "./chunk-VPC3QSPW.mjs";
+import "./chunk-SZXICFQ3.mjs";
+import "./chunk-UCMJJ3IM.mjs";
+import "./chunk-J7LXIPZS.mjs";
+init_routing();
+export {
+  checkLoopBudget,
+  evaluateGoto,
+  evaluateTransitions,
+  handleRouting
+};
+//# sourceMappingURL=routing-ODVQHICC.mjs.map
--- dist/sdk/sdk.mjs
+++ dist/sdk/sdk.mjs
@@ -1,7 +1,7 @@
 import {
   StateMachineExecutionEngine,
   init_state_machine_execution_engine
-} from "./chunk-AADKUA6L.mjs";
+} from "./chunk-U4ZJWL63.mjs";
 import "./chunk-KFKHU6CM.mjs";
 import "./chunk-M3BYMES6.mjs";
 import "./chunk-LG4AUKHB.mjs";
@@ -14,9 +14,9 @@ import {
   init_config
 } from "./chunk-FYK2DJK6.mjs";
 import "./chunk-NCWIZVOT.mjs";
-import "./chunk-GLROSEYJ.mjs";
-import "./chunk-FZEQ744M.mjs";
-import "./chunk-PETLPNRA.mjs";
+import "./chunk-CZOKADR7.mjs";
+import "./chunk-LH3PEGKM.mjs";
+import "./chunk-NE3OOOB2.mjs";
 import "./chunk-JL7JXCET.mjs";
 import "./chunk-ZUEQNCKB.mjs";
 import "./chunk-25IC7KXZ.mjs";
